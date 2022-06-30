6fccfc5905435352d598c3904addd03d8c247bb5
vishalcangit <vishalsuvarna000@gmail.com>
2022-06-30 15:26:58 +0530

    project done in react

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
+import { useState } from "react";
 
 function App() {
+
+  const options = [
+    { value: 'yes', label: 'Yes' },
+    { value: 'no', label: 'Probably Not' }
+  ];
+
+  const [value, setValue] = useState('');
+
+  const handleChange = (e) => {
+    setValue(e.target.value)
+  }
   return (
     <div className="dropdown">
       <h1>
         Should you use a dropdown?
       </h1>
-      <select>
+      <select onChange={handleChange} value={value}>
         <option value="" disabled selected>Select</option>
-        <option value="yes">Yes</option>
-        <option value="no" >Probably Not</option>
+        {options.map((option) => (
+          <option value={option.value}>{option.label}</option>
+        ))}
       </select>
     </div>
   );
